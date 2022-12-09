54b482d3a62d9283040082a129c8ed7de2b94973
Arif <80994072+arf-themascoteers@users.noreply.github.com>
2022-12-10 00:49:07 +1100

    Initial commit

---
+++ vb_linear.py
@@ -0,0 +1,22 @@
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sm_dataset import SM_Dataset
+
+
+def linear():
+    ds = SM_Dataset(is_train=True)
+    reg = LinearRegression().fit(ds.X, ds.y)
+    ds = SM_Dataset(is_train=False)
+    y_hat = reg.predict(ds.X, ds.y)
+    r2 = r2_score(ds.y, y_hat)
+    mae = mean_absolute_error(ds.y, y_hat)
+    mse = mean_squared_error(ds.y, y_hat)
+
+    return r2, mae, mse
+
+
+if __name__ == "__main__":
+    r2, mae, mse = linear()
+    print(r2, mae, mse)
